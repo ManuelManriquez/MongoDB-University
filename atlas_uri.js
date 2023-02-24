d6ed9832357fc37b37ad4ef7e029b3f458eee023
Manuel Manriquez <manuel.manriquez.e@gmail.com>
2023-02-24 15:45:45 -0700

    dotenv configuration

--- atlas_uri.js
+++ atlas_uri.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+
 module.exports = uri =
-  `mongodb+srv://${process.ENV.User}:${process.ENV.Password}@cluster0.r8rvhhl.mongodb.net/?retryWrites=true&w=majority`;
+  `mongodb+srv://${process.env.dbUSER}:${process.env.dbPASS}@cluster0.r8rvhhl.mongodb.net/?retryWrites=true&w=majority`;
+  
   
