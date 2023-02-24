5c7b3daa974c5190fd83da4806ff0548e722c105
Manuel Manriquez <manuel.manriquez.e@gmail.com>
2023-02-24 15:47:48 -0700

    cleaning fle

--- atlas_uri.js
+++ atlas_uri.js
@@ -2,5 +2,3 @@ require("dotenv").config();
 
 module.exports = uri =
   `mongodb+srv://${process.env.dbUSER}:${process.env.dbPASS}@cluster0.r8rvhhl.mongodb.net/?retryWrites=true&w=majority`;
-  
-  
